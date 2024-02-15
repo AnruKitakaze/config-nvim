cd5ad8326bee041d0400c80a884e57788562b5f9
Anru Kitakaze <42811808+AnruKitakaze@users.noreply.github.com>
2024-02-15 19:21:22 +0700

    feat(colorschemes): add vscode like colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,17 +2,28 @@ return {
   -- the colorscheme should be available when starting Neovim
     {
         "folke/tokyonight.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
+--        config = function()
+--            -- load the colorscheme here
+--            vim.cmd([[colorscheme tokyonight]])
+--        end,
     },
     {
         "rose-pine/neovim", 
         name = "rose-pine",
+--        config = function()
+--            -- load the colorscheme here
+--            vim.cmd([[colorscheme rose-pine]])
+--        end,
     },
     {
-        "catppuccin/nvim", 
-        name = "catppuccin", 
-        priority = 1000 
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+--        config = function()
+--            -- load the colorscheme here
+--            vim.cmd([[colorscheme catppuccin]])
+--        end,
     },
     { 
         "luisiacc/gruvbox-baby",
@@ -22,4 +33,12 @@ return {
             vim.cmd([[colorscheme gruvbox-baby]])
         end,
     },
+    {
+        "Mofiqul/vscode.nvim",
+--        priority = 1000,
+--        config = function()
+--            vim.cmd([[colorscheme vscode]])
+--        end,
+    },
+
 }
